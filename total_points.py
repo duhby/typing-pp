f9c66a500aa60f5ec78467b5eb487d39d3f7277b
dubs <59372145+duhby@users.noreply.github.com>
2022-07-21 19:26:46 -0600

    Create total_points.py

---
+++ total_points.py
@@ -0,0 +1,21 @@
+# How to calculate a player's total points given all of their plays
+
+# List of their top plays in order (real values will be to 2 decimal places)
+example_player = [210, 200, 190, 180, 170, 160, 155, 140, 122, 120, 110, 109, 108, 107, 106, 103, 102, 102, 101, 100, 99]
+
+weighted_points = []
+
+# n is the placement, points is the current item in the list
+for n, points in enumerate(example_player):
+    n += 1 # placement should start at 1; enumerate starts at 0
+    weighted = points * .97**(n-1)
+    weighted_points.append(weighted)
+
+# Adds weighted points together
+total_points = sum(weighted_points)
+
+print(total_points) # 2196.18 (rounded)
+
+# As opposed to
+
+print(sum(example_player)) # 2794
